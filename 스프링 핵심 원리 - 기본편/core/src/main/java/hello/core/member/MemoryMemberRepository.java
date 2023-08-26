f676e0910b1e935b07042c60443f74890a58c1d9
goghrf <daynnite9924@gmail.com>
2023-08-26 17:04:36 +0900

    주문과 할인 도메인 실행과 테스트

--- 스프링 핵심 원리 - 기본편/core/src/main/java/hello/core/member/MemoryMemberRepository.java
+++ 스프링 핵심 원리 - 기본편/core/src/main/java/hello/core/member/MemoryMemberRepository.java
@@ -5,6 +5,8 @@ import java.util.Map;
 
 public class MemoryMemberRepository implements MemberRepository{
 
+    // 메모리 Repository라는 게
+    // Map에 데이터를 저장해놓겠다는 의미
     private static Map<Long, Member> store = new HashMap<>();
 
     @Override

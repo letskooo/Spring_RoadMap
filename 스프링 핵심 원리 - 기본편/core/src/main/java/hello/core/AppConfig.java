c366802785a83e93e4b8c61837f2a225c451040c
goghrf <daynnite9924@gmail.com>
2023-08-26 20:22:44 +0900

    새로운 구조와 할인 정책 적용

--- 스프링 핵심 원리 - 기본편/core/src/main/java/hello/core/AppConfig.java
+++ 스프링 핵심 원리 - 기본편/core/src/main/java/hello/core/AppConfig.java
@@ -24,6 +24,6 @@ public class AppConfig {
         return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
     public DiscountPolicy discountPolicy(){
-        return new FixDiscountPolicy();
+        return new RateDiscountPolicy();
     }
 }

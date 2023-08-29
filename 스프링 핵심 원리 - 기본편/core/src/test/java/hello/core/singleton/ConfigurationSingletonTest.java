ae0a5456721380c4f834f4c88b4fa9712971ff4f
goghrf <daynnite9924@gmail.com>
2023-08-29 16:09:51 +0900

    컴포넌트 스캔과 의존관계 자동 주입 시작하기

--- 스프링 핵심 원리 - 기본편/core/src/test/java/hello/core/singleton/ConfigurationSingletonTest.java
+++ 스프링 핵심 원리 - 기본편/core/src/test/java/hello/core/singleton/ConfigurationSingletonTest.java
@@ -6,6 +6,7 @@ import hello.core.member.MemberServiceImpl;
 import hello.core.order.OrderServiceImpl;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class ConfigurationSingletonTest {
@@ -28,5 +29,15 @@ public class ConfigurationSingletonTest {
 
         Assertions.assertThat(memberRepository1).isSameAs(memberRepository);
         Assertions.assertThat(memberRepository2).isSameAs(memberRepository);
+    }
+
+    @Test
+    void configurationDeep(){
+
+        ApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+        AppConfig bean = ac.getBean(AppConfig.class);
+
+        System.out.println("bean = " + bean.getClass());
+
     }
 }

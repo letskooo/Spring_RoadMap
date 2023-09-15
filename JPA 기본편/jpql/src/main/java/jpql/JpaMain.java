e5efb536b9e3dcbfea95e4918b39d75a67a525d0
goghrf <daynnite9924@gmail.com>
2023-09-15 19:27:23 +0900

    기본 문법과 쿼리 API

---
+++ JPA 기본편/jpql/src/main/java/jpql/JpaMain.java
@@ -0,0 +1,34 @@
+package jpql;
+
+import javax.persistence.*;
+import java.util.List;
+
+public class JpaMain {
+    public static void main(String[] args) {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+        EntityManager em = emf.createEntityManager();
+        EntityTransaction tx = em.getTransaction();  // 트랜잭션 얻기
+        tx.begin();  // 트랜잭션 시작
+
+        try {
+
+            Member member = new Member();
+            member.setUsername("member1");
+            member.setAge(10);
+            em.persist(member);
+
+            Member result = em.createQuery("select m from Member m where m.username = :username", Member.class)
+                            .setParameter("username", "member1")
+                                    .getSingleResult();
+            System.out.println("singleResult = " + result.getUsername());
+
+            tx.commit();
+        } catch (Exception e){
+            tx.rollback();
+            e.printStackTrace();
+        } finally {
+            em.close();
+        }
+        emf.close();
+    }
+}

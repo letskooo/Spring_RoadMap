cd61c8e04e8f46454ab08e0b743b058bc575bc8c
goghrf <daynnite9924@gmail.com>
2023-09-15 21:20:43 +0900

    페이징

--- JPA 기본편/jpql/src/main/java/jpql/Member.java
+++ JPA 기본편/jpql/src/main/java/jpql/Member.java
@@ -32,4 +32,12 @@ public class Member {
     public void setAge(int age) {
         this.age = age;
     }
+    @Override
+    public String toString(){
+        return "Member{" +
+                "id=" + id +
+                ", username= '" + username + '\'' +
+                ", age=" + age +
+                '}';
+    }
 }

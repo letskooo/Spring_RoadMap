b56dd310e803ec8587b735a10bdb3b5e89d8e18f
goghrf <daynnite9924@gmail.com>
2023-09-16 18:46:52 +0900

    조건부 평가

--- 스프링 MVC 2/thymeleaf-basic/src/main/java/hello/thymeleaf/basic/BasicController.java
+++ 스프링 MVC 2/thymeleaf-basic/src/main/java/hello/thymeleaf/basic/BasicController.java
@@ -116,4 +116,10 @@ public class BasicController {
 
         model.addAttribute("users", list);
     }
+
+    @GetMapping("/condition")
+    public String condition(Model model) {
+        addUsers(model);
+        return "basic/condition";
+    }
 }

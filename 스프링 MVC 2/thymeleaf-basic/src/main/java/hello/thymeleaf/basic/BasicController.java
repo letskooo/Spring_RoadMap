d0c520bd805476d46c773f8e4f4a3a88048c7dc0
goghrf <daynnite9924@gmail.com>
2023-09-16 19:31:18 +0900

    자바스크립트 인라인

--- 스프링 MVC 2/thymeleaf-basic/src/main/java/hello/thymeleaf/basic/BasicController.java
+++ 스프링 MVC 2/thymeleaf-basic/src/main/java/hello/thymeleaf/basic/BasicController.java
@@ -133,4 +133,10 @@ public class BasicController {
         addUsers(model);
         return "basic/block";
     }
+    @GetMapping("/javascript")
+    public String javascript(Model model){
+        model.addAttribute("user", new User("UserA", 10));
+        addUsers(model);
+        return "basic/javascript";
+    }
 }

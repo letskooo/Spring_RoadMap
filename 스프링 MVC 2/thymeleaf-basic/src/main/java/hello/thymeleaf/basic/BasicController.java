08e8f1e2a2cc434f3f9a81f2d1011c56f7883885
goghrf <daynnite9924@gmail.com>
2023-09-16 19:02:07 +0900

    블록

--- 스프링 MVC 2/thymeleaf-basic/src/main/java/hello/thymeleaf/basic/BasicController.java
+++ 스프링 MVC 2/thymeleaf-basic/src/main/java/hello/thymeleaf/basic/BasicController.java
@@ -128,4 +128,9 @@ public class BasicController {
         model.addAttribute("data", "Spring!");
         return "basic/comments";
     }
+    @GetMapping("/block")
+    public String block(Model model) {
+        addUsers(model);
+        return "basic/block";
+    }
 }

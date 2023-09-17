1b718e65da3f65120aaa854aac57070c85aebf06
goghrf <daynnite9924@gmail.com>
2023-09-17 12:59:04 +0900

    Hello 서블릿

---
+++ 스프링 MVC 1/servlet/src/main/java/hello/servlet/basic/HelloServlet.java
@@ -0,0 +1,31 @@
+package hello.servlet.basic;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+@WebServlet(name = "helloServlet", urlPatterns = "/hello")
+public class HelloServlet extends HttpServlet {
+
+    @Override
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        super.service(request, response);
+
+        System.out.println("HelloServlet.service");
+        System.out.println("request = " + request);
+        System.out.println("response = " + response);
+
+        String username = request.getParameter("username");
+        System.out.println("username = " + username);
+
+        response.setContentType("text/plain");  // HTTP 응답 메시지 헤더에 들어감
+        response.setCharacterEncoding("utf-8"); // HTTP 응답 메시지 헤더에 들어감
+        response.getWriter().write("hello" + username); // HTTP 메시지 바디에 내용이 들어감
+    }
+}

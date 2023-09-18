3c7fca1a2037aa11a9636861e965617a51cec35e
goghrf <daynnite9924@gmail.com>
2023-09-18 20:27:46 +0900

    RedirectAttributes

--- 스프링 MVC 1/item-service/src/main/java/hello/itemservice/web/basic/BasicItemController.java
+++ 스프링 MVC 1/item-service/src/main/java/hello/itemservice/web/basic/BasicItemController.java
@@ -7,6 +7,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import javax.annotation.PostConstruct;
 import java.util.List;
@@ -87,11 +88,22 @@ public class BasicItemController {
 //        return "basic/item";
 //    }
 
+//    @PostMapping("/add")
+//    public String addItemV5(Item item){
+//        itemRepository.save(item);
+//        return "redirect:/basic/items/" + item.getId();
+//    }
+
     @PostMapping("/add")
-    public String addItemV5(Item item){
-        itemRepository.save(item);
-        return "redirect:/basic/items/" + item.getId();
+    public String addItemV6(Item item, RedirectAttributes redirectAttributes){
+        Item savedItem = itemRepository.save(item);
+        // 리다이렉트랑 관련된 속성들을 넣는 것
+        redirectAttributes.addAttribute("itemId", savedItem.getId());
+        redirectAttributes.addAttribute("status", true);
+        return "redirect:/basic/items/{itemId}";
+
     }
+
 
     @GetMapping("/{itemId}/edit")
     public String editForm(@PathVariable Long itemId, Model model){

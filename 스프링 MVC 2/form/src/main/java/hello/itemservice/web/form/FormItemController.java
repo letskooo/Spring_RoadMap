f07b3b2e03c0c916587a971377a59f73b3567fee
goghrf <daynnite9924@gmail.com>
2023-09-19 01:38:03 +0900

    라디오 버튼

--- 스프링 MVC 2/form/src/main/java/hello/itemservice/web/form/FormItemController.java
+++ 스프링 MVC 2/form/src/main/java/hello/itemservice/web/form/FormItemController.java
@@ -2,6 +2,7 @@ package hello.itemservice.web.form;
 
 import hello.itemservice.domain.item.Item;
 import hello.itemservice.domain.item.ItemRepository;
+import hello.itemservice.domain.item.ItemType;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -30,6 +31,12 @@ public class FormItemController {
         regions.put("JEJU", "제주");
         return regions;
     }
+
+    @ModelAttribute("itemTypes")
+    public ItemType[] itemTypes() {
+        return ItemType.values();
+    }
+
 
     @GetMapping
     public String items(Model model) {

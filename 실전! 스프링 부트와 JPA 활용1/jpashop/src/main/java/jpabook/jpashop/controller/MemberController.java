7b6d596192dd5fa9d6f44da7ed7ddb9662b8f015
goghrf <daynnite9924@gmail.com>
2023-09-21 15:36:41 +0900

    회원 목록 조회

--- 실전! 스프링 부트와 JPA 활용1/jpashop/src/main/java/jpabook/jpashop/controller/MemberController.java
+++ 실전! 스프링 부트와 JPA 활용1/jpashop/src/main/java/jpabook/jpashop/controller/MemberController.java
@@ -10,6 +10,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.List;
+
 @Controller
 @RequiredArgsConstructor
 public class MemberController {
@@ -32,5 +34,13 @@ public class MemberController {
 
         memberService.join(member);
         return "redirect:/";
+    }
+
+    @GetMapping("/members")
+    public String list(Model model){
+
+        List<Member> members = memberService.findMembers();
+        model.addAttribute("members", members);
+        return "members/memberList";
     }
 }

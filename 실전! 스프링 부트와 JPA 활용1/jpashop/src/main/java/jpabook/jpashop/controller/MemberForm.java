dff4efefb50d2bd344a20af08c567a262b60b8f5
goghrf <daynnite9924@gmail.com>
2023-09-21 15:15:38 +0900

    회원 등록

---
+++ 실전! 스프링 부트와 JPA 활용1/jpashop/src/main/java/jpabook/jpashop/controller/MemberForm.java
@@ -0,0 +1,17 @@
+package jpabook.jpashop.controller;
+
+import com.sun.istack.NotNull;
+import lombok.Getter;
+import lombok.NonNull;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class MemberForm {
+
+    private String name;
+
+    private String city;
+    private String street;
+    private String zipcode;
+}

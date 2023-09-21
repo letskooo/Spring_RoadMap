aa40b4341bee9845b6bf033e109263d5babce01d
goghrf <daynnite9924@gmail.com>
2023-09-21 19:42:11 +0900

    변경 감지와 병합(merge)

--- 실전! 스프링 부트와 JPA 활용1/jpashop/src/main/java/jpabook/jpashop/controller/ItemController.java
+++ 실전! 스프링 부트와 JPA 활용1/jpashop/src/main/java/jpabook/jpashop/controller/ItemController.java
@@ -65,18 +65,18 @@ public class ItemController {
     }
 
     @PostMapping("/items/{itemId}/edit")
-    public String updateItem(@ModelAttribute BookForm form){
+    public String updateItem(@PathVariable Long itemId, @ModelAttribute BookForm form){
 
-        Book book = new Book();
+//        Book book = new Book();
+//
+//        book.setId(form.getId());
+//        book.setName(form.getName());
+//        book.setPrice(form.getPrice());
+//        book.setStockQuantity(form.getStockQuantity());
+//        book.setAuthor(form.getAuthor());
+//        book.setIsbn(form.getIsbn());
 
-        book.setId(form.getId());
-        book.setName(form.getName());
-        book.setPrice(form.getPrice());
-        book.setStockQuantity(form.getStockQuantity());
-        book.setAuthor(form.getAuthor());
-        book.setIsbn(form.getIsbn());
-
-        itemService.saveItem(book);
+        itemService.updateItem(itemId, form.getName(), form.getPrice(), form.getStockQuantity());
         return "redirect:/items";
 
     }

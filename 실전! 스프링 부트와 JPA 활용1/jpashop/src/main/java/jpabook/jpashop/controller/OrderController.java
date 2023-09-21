5bb7dac49fcbea63cc06721ca08c45e6e99fd04c
goghrf <daynnite9924@gmail.com>
2023-09-21 21:20:35 +0900

    주문 목록 검색, 취소

--- 실전! 스프링 부트와 JPA 활용1/jpashop/src/main/java/jpabook/jpashop/controller/OrderController.java
+++ 실전! 스프링 부트와 JPA 활용1/jpashop/src/main/java/jpabook/jpashop/controller/OrderController.java
@@ -1,17 +1,16 @@
 package jpabook.jpashop.controller;
 
 import jpabook.jpashop.domain.Member;
+import jpabook.jpashop.domain.Order;
 import jpabook.jpashop.domain.item.Item;
+import jpabook.jpashop.repository.OrderSearch;
 import jpabook.jpashop.service.ItemService;
 import jpabook.jpashop.service.MemberService;
 import jpabook.jpashop.service.OrderService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -35,13 +34,25 @@ public class OrderController {
         return "order/orderForm";
 
     }
-
     @PostMapping("/order")
     public String order(@RequestParam Long memberId,
                         @RequestParam Long itemId,
                         @RequestParam int count){
 
         orderService.order(memberId, itemId, count);
+        return "redirect:/orders";
+    }
+
+    @GetMapping("/orders")
+    public String orderList(@ModelAttribute("orderSearch") OrderSearch orderSearch, Model model){
+        List<Order> orders = orderService.findOrders(orderSearch);
+        model.addAttribute("orders", orders);
+        return "order/orderList";
+    }
+
+    @PostMapping("/orders/{orderId}/cancel")
+    public String cancleOrder(@PathVariable("orderId") Long orderId){
+        orderService.cancelOrder(orderId);
         return "redirect:/orders";
     }
 }

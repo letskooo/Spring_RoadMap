7176885782cbc7e8e9b924c5253493ce63756443
goghrf <daynnite9924@gmail.com>
2023-09-28 19:55:02 +0900

    스프링 인터셉터 - 인증 체크

--- 스프링 MVC 2/login/src/main/java/hello/login/WebConfig.java
+++ 스프링 MVC 2/login/src/main/java/hello/login/WebConfig.java
@@ -2,28 +2,39 @@ package hello.login;
 
 import hello.login.web.filter.LogFilter;
 import hello.login.web.filter.LoginCheckFilter;
+import hello.login.web.interceptor.LogInterceptor;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import javax.servlet.Filter;
 import javax.servlet.FilterRegistration;
 
 @Configuration
-public class WebConfig {
+public class WebConfig implements WebMvcConfigurer {
 
-    @Bean
-    public FilterRegistrationBean logFilter(){
-        FilterRegistrationBean<Filter> filterRegistrationBean = new FilterRegistrationBean<>();
-        // 직접 만든 LogFilter를 넣어줌
-        filterRegistrationBean.setFilter(new LogFilter());
-        // 필터 체인 중에서 해당 필터의 순서 정하기
-        filterRegistrationBean.setOrder(1);
-        // 어떤 URL 패턴에 필터를 적용시킬거냐
-        filterRegistrationBean.addUrlPatterns("/*");
-
-        return filterRegistrationBean;
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new LogInterceptor())
+                .order(1)   // 인터셉터 필터 중 순서 1번
+                .addPathPatterns("/**") // 하위 전부다
+                .excludePathPatterns("/css/**", "/*.ico", "/error");  // 여기는 인터셉터 적용하지마
     }
+
+//    @Bean
+//    public FilterRegistrationBean logFilter(){
+//        FilterRegistrationBean<Filter> filterRegistrationBean = new FilterRegistrationBean<>();
+//        // 직접 만든 LogFilter를 넣어줌
+//        filterRegistrationBean.setFilter(new LogFilter());
+//        // 필터 체인 중에서 해당 필터의 순서 정하기
+//        filterRegistrationBean.setOrder(1);
+//        // 어떤 URL 패턴에 필터를 적용시킬거냐
+//        filterRegistrationBean.addUrlPatterns("/*");
+//
+//        return filterRegistrationBean;
+//    }
 
     @Bean
     public FilterRegistrationBean loginCheckFilter(){

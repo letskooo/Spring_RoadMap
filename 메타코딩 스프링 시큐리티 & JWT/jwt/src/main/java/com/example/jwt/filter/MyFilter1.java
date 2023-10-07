02a1e1f32f257c18c4113da8d4caf4d20f80d14c
goghrf <daynnite9924@gmail.com>
2023-10-07 19:53:00 +0900

    JWT 임시 토큰 만들어서 테스트 해보기

--- 메타코딩 스프링 시큐리티 & JWT/jwt/src/main/java/com/example/jwt/filter/MyFilter1.java
+++ 메타코딩 스프링 시큐리티 & JWT/jwt/src/main/java/com/example/jwt/filter/MyFilter1.java
@@ -1,9 +1,13 @@
 package com.example.jwt.filter;
 
 import jakarta.servlet.*;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.http.HttpResponse;
 
 @Slf4j
 public class MyFilter1 implements Filter {
@@ -12,9 +16,7 @@ public class MyFilter1 implements Filter {
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
             throws IOException, ServletException {
 
-        log.info("===========filter1==============");
+        log.info("============filter1============");
         chain.doFilter(request, response);
-
-
     }
 }
--- 메타코딩 스프링 시큐리티 & JWT/jwt/src/main/java/com/example/jwt/filter/MyFilter3.java
+++ 메타코딩 스프링 시큐리티 & JWT/jwt/src/main/java/com/example/jwt/filter/MyFilter3.java
@@ -1,9 +1,12 @@
 package com.example.jwt.filter;
 
 import jakarta.servlet.*;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 
 @Slf4j
 public class MyFilter3 implements Filter {
@@ -12,7 +15,22 @@ public class MyFilter3 implements Filter {
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
             throws IOException, ServletException {
 
-        log.info("===========filter3==============");
-        chain.doFilter(request, response);
+        HttpServletRequest req = (HttpServletRequest) request;
+        HttpServletResponse resp = (HttpServletResponse) response;
+
+        if (req.getMethod().equals("POST")){
+            log.info("POST 요청됨");
+            String headerAuth = req.getHeader("Authorization"); // 리턴 타입은 String
+
+            log.info(headerAuth);
+            log.info("===========filter3=============");
+
+            if (headerAuth.equals("cos")){
+                chain.doFilter(req, resp);
+            } else {
+                PrintWriter outPrintWriter = resp.getWriter();
+                outPrintWriter.println("no pass");
+            }
+        }
     }
 }

b2f73f61a977577c3ffbcc868d68b0977d0729f2
goghrf <daynnite9924@gmail.com>
2023-10-14 15:31:45 +0900

    예제 도메인 모델과 동작확인

--- 실전! 스프링 데이터 JPA/data-jpa/src/main/java/study/datajpa/entity/Member.java
+++ 실전! 스프링 데이터 JPA/data-jpa/src/main/java/study/datajpa/entity/Member.java
@@ -1,30 +1,42 @@
 package study.datajpa.entity;
 
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Entity
 @Getter
 @Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@ToString(of = {"id", "username", "age"})
 public class Member {
 
     @Id
     @GeneratedValue
+    @Column(name = "MEMBER_ID")
     private Long id;
     private String username;
+    private int age;
 
-    // JPA는 기본 생성자가 있어야 하는데
-    // 다른 곳에서 호출되지 않도록 protected 접근자 설정
-    protected Member(){
-
-    }
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "TEAM_ID")
+    private Team team;
 
     public Member(String username){
         this.username = username;
+    }
+
+    public Member(String username, int age, Team team){
+        this.username = username;
+        this.age = age;
+
+        if (team != null) {
+            changeTeam(team);
+        }
+    }
+
+    public void changeTeam(Team team){
+        this.team = team;
+        team.getMembers().add(this);
     }
 }
